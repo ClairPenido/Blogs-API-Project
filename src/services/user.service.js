a68e48abb7f3ae25b038ce5109c30fe338ea8a0c
Clair Penido <clairpenido@gmail.com>
2022-09-23 20:44:31 -0300

    tentando fazer o lance dos erros

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,19 +1,16 @@
 const { User } = require('../models');
+const errorGenerate = require('../utils/genericError');
 
 const createUser = async (req, res) => {
-  try {
     const { displayName, email, password, image } = req.body;
     const enterLogin = await User.create({ displayName, email, password, image });
 
-    if (!enterLogin) {
-      return res.status(404).send({ message: 'Erro ao cadastrar uma conta!' });
-    }
+    if (enterLogin.displayName.lenght < 8) {
+      throw errorGenerate(400,
+         '"displayName" length must be at least 8 characters long', 'string.min');
+    } 
     
     return res.status(200).json({ message: 'Conta cadastrada com sucesso' });
-  } catch (err) {
-    console.log('qual o erro:', err.message);
-    res.status(500).json({ message: 'Algo deu errado!' });
-  }
 };
 
 module.exports = {
